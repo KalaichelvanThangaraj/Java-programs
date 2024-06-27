6a0dd6ecc4e5571e934edaab661e7171acd1a8d9
Kalaichelvan T <163325793+KalaichelvanThangaraj@users.noreply.github.com>
2024-06-27 14:13:19 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class count {
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the Number:");
+    int n=sc.nextInt();
+    String str=Integer.toString(n);
+    System.out.print("digit present in the number is:");
+    System.out.println(str.length());
+    
+    }
+    
+}
+
+
+/* 
+ Output:
+ Enter the Number:6532568
+digit present in the number is:7
+ */
